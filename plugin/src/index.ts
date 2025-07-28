fe8d7bee24d5d1e15dbcb727d6e8fc09d97ed695
Josh Nussbaum <joshnuss@gmail.com>
2025-07-28 19:47:00 -0400

    Adds write_definitions() function

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hey')
+import type { PluggableList } from 'unified'
+
+export type Options<Schema> = {
+  base: string
+  pattern: string
+  fields?: Schema
+  plugins?: {
+    remark?: PluggableList
+    rehype?: PluggableList
+  }
+  sort?: {
+    field: string
+    order?: 'ascending' | 'descending'
+  }
+}
+
+export type Type = {
+  name: string
+  type: string
+  optional: boolean
+}
