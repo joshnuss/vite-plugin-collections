12159d86e5eb51bc2976e7f8930e30f1a9113fcb
Josh Nussbaum <joshnuss@gmail.com>
2025-07-28 23:59:47 -0400

    Add plugin logic

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,5 +1,6 @@
 import path from 'path'
-import { reset_types } from './typing'
+import { extract_types, write_definitions, reset_types } from './typing.js'
+import { parse_markdown } from './markdown.js'
 import type { Plugin } from 'vite'
 import type { TransformResult } from 'rollup'
 import type { PluggableList } from 'unified'
@@ -22,6 +23,9 @@ export type Options = {
 const definitions_path = 'collection.d.ts'
 
 export default function collection(options: Options): Plugin {
+  const root_path = path.join('src', options.base)
+  const pattern = options.pattern || '*.md'
+
   return {
     name: 'vite-plugin-collections',
     async config() {
@@ -30,10 +34,99 @@ export default function collection(options: Options): Plugin {
       return {
         resolve: {
           alias: {
-            [`#${options.base}`]: path.resolve(`./src/${options.base}`)
+            [`#${options.base}`]: path.resolve(`./${root_path}`)
           }
         }
       }
-    }
+    },
+
+    resolveId(id) {
+      if (id.endsWith(root_path)) {
+        return id
+      }
+    },
+
+    load(id) {
+      if (id.endsWith(root_path)) {
+        return `
+          export function get(id) {
+            return import(\`./${options.base}/\${id}.md\`)
+          }
+
+          export async function list() {
+            const files = import.meta.glob('./${options.base}/${pattern}')
+            const records = await Promise.all(
+              Object
+                .values(files)
+                .map((content) => content())
+            )
+            return records${build_sort(options.sort)}
+          }
+        `
+      }
+    },
+
+    async transform(content, file): Promise<TransformResult> {
+      if (!path.extname(file).match(/^\.md/)) {
+        return
+      }
+
+      const vfile = await parse_markdown(content, options.plugins?.remark || [], options.plugins?.rehype || [])
+
+      let attributes: ZodObject | unknown
+
+      if (options.fields) {
+        attributes = options.fields.parse(vfile.data.matter)
+      }
+
+      let constants: string[] = []
+
+      if (attributes) {
+        constants = Object.entries(attributes).map(
+          ([key, value]) => `export const ${key} = ${JSON.stringify(value)}`
+        )
+      }
+
+      return {
+        code: `
+          export const id = "${path.basename(file, '.md')}"
+          export const body = \`${vfile.value}\`
+          ${constants.join('\n')}
+        `
+      }
+    },
+
+    async buildStart() {
+      const typings = await extract_types(options.fields)
+      await write_definitions(definitions_path, options.base, typings)
+    },
   }
+}
+
+function build_sort(sort: Options['sort']): string {
+  if (!sort) return ''
+
+  const {field, order} = sort
+
+  if (order == 'descending') {
+    return `.sort((a, b) => {
+      if (a.${field} > b.${field}) {
+        return -1
+      }
+      if (a.${field} < b.${field}) {
+        return 1
+      }
+      return 0
+    })`
+  }
+
+  return `.sort((a, b) => {
+    if (a.${field} < b.${field}) {
+      return -1
+    }
+    if (a.${field} > b.${field}) {
+      return 1
+    }
+    return 0
+  })`
 }
